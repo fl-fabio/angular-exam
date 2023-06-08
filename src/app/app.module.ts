3753b4e0d3626e69408c6b008a3e79831f17f6a5
fl-fabio <fl.fabio.lanzafame@gmail.com>
2023-06-08 15:14:31 +0200

    feature(): create first project scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/public/login/login.component';
+import { FavoriteComponent } from './pages/private/favorite/favorite.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FavoriteComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
