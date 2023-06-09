956b2f3db9254d3572c64a3ba938eea1e19d4052
fl-fabio <fl.fabio.lanzafame@gmail.com>
2023-06-09 12:39:53 +0200

    feature(): subject and observable on auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,23 +1,41 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private currentUserSubject: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(null);
+  public currentUser$: Observable<string | null> = this.currentUserSubject.asObservable();
+
   private email: string = 'admin@admin.com';
   private password: string = 'Bls0906';
+  private user: string | null = null;
   isAuthenticated: boolean = false;
 
-  login(username: string, password: string) {
+  login = (email: string, password: string): boolean => {
+    const isAuthenticated = this.authenticateUser(email, password);
+
+    if (isAuthenticated) {
+      localStorage.setItem('currentUser', email.split("@")[0]);
+      this.currentUserSubject.next(email.split("@")[0]);
+    }
+
+    return isAuthenticated;
+
+  }
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+
+  private authenticateUser(username: string, password: string): boolean {
     if(username===this.email && password === this.password) {
-      localStorage.setItem('currentUser', this.email.split("@")[0]);
+      this.user = this.email.split("@")[0];
+      localStorage.setItem('currentUser', this.user);
       return true;
     } 
     return false;
-  }
-
-  logout() {
-    localStorage.removeItem('currentUser');
   }
 
 
